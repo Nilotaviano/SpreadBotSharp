0b80476929fc3366e25f11d1f76acbc74a04ff55
Cleivson Arruda <cleivson.tb@gmail.com>
2020-12-08 17:01:51 -0300

    fix SpreadThresholdBeforeCancelingCurrentOrder when selling

--- SpreadBot/Logic/BotStrategies/Spread/SpreadSellOrderActiveStateStrategy.cs
+++ SpreadBot/Logic/BotStrategies/Spread/SpreadSellOrderActiveStateStrategy.cs
@@ -13,7 +13,7 @@ namespace SpreadBot.Logic.BotStrategies.Spread
     {
         public async Task ProcessMarketData(IExchange exchange, SpreadConfiguration spreadConfiguration, Stopwatch buyStopwatch, decimal balance, decimal heldAmount, Func<Func<Task<OrderData>>, Task> executeOrderFunctionCallback, Action finishWorkCallBack, OrderData currentOrderData, MarketData marketData, decimal boughtPrice)
         {
-            if (currentOrderData.Limit - marketData.AskRate > spreadConfiguration.SpreadThresholdBeforeCancelingCurrentOrder)
+            if (currentOrderData.Limit - marketData.AskRate >= spreadConfiguration.SpreadThresholdBeforeCancelingCurrentOrder)
             {
                 //cancel order and switch to BotState.Sell
                 //TODO: I think we should rate limit how often we cancel orders here
